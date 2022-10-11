ece46776827d47a82d5a8ca77cb493ca957e23a2
rayyan ahsan <raygraphicsreal@gmail.com>
2022-10-11 23:01:27 +0100

    passed pytest

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if(number_of_primes < 1):
+        raise ValueError("Please enter a value that is equal to or higher than 1.")
     list = []
+    x = 2
+    while number_of_primes != 0:
+        for q in range(2,x):
+            if x % q == 0:
+                break
+        else:
+            list.append(x)
+            number_of_primes = number_of_primes - 1
+        x = x + 1
     return list
